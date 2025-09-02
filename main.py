d7c43e2624570254d6015e1219193e1d82d7cec9
Oleg Oleksuk <83314623+olegolkk@users.noreply.github.com>
2025-09-02 13:42:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# Меняем запуск в конце
+if __name__ == "__main__":
+    import uvicorn
 
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
 class ConnectionManager:
     def __init__(self):
         self.active_connections: Dict[str, WebSocket] = {}
